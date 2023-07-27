59dd0e0ecd7560fc3197a40d72bbdc2a8c822f32
Ckalimero <136835675+Ckalimero@users.noreply.github.com>
2023-07-27 17:57:53 +0200

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -1,14 +1,9 @@
-def num_primo (num):
-    d=2
-    list1=[]
-    while d<=num:
-        if num%d==0:
-            num=num/d
-            list1.append(d)
-        else:
-            d +=1
-    return list1
-        
-resultado=int(input("Dame un numero: "))
-salida=num_primo(resultado)
-print (salida)
+reglas = {
+        "PIEDRA": "TIJERAS",
+        "TIJERAS": "PAPEL",
+        "PAPEL": "PIEDRA"
+    }
+
+print(reglas["PIEDRA"])
+print(reglas["TIJERAS"])
+print(reglas["PAPEL"])
